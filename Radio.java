160c5f593804f51ccee201f78a3e40cf49216c82
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2020-01-24 12:40:37 -0600

    Add files via upload

--- Radio.java
+++ Radio.java
@@ -11,11 +11,20 @@ Y FM que van de 87.9 a 107.9 en intervalos de 0.2 en 0.2
 
 Last modification: 20/01/2020
 */
+/**
+ * @author Marco Ramirez 19588
+ * @author Julio Herrera 19402
+ * @version 2.0
+ * 
+ * 
+ */
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-public class Radio implements RadioInterface {
-
+public class Radio implements iRadio {
+	
+	
+	
     private boolean onOff; //guarda el estado de encendido o apagado
     private boolean amFm; //guarda el estado de AM/FM FALSE = AM / TRUE = FM
     private double estacionActual; //guarda la estacion actual
@@ -23,9 +32,9 @@ public class Radio implements RadioInterface {
     private ArrayList<Double> estacionesFm; //Botones de estaciones FM
     private static DecimalFormat df = new DecimalFormat("#.#");
     
-    public Radio() {
-        onOff = false; // <-- Apagado
-        amFm = false; // <-- Am
+    public  Radio() {
+        onOff = false; //<-- Apagado
+        amFm = false; //<-- Am
         estacionActual = 530.0; //Inicio de las estaciones
         estacionesAm = new ArrayList<Double>(12);
         estacionesFm = new ArrayList<Double>(12);
@@ -35,13 +44,29 @@ public class Radio implements RadioInterface {
         }
     }
 
+    /**
+     * @param estacionActual nos indica la estación actual
+     * @return convierte la estacion a String
+     * 
+     */
     public String estacionActual() {
         return df.format(this.estacionActual); // convierte a string para mostrar en label
     }
+    
+    /***
+     * @param estado indica si este es verdadero o falso
+     * @return regresa el estado que posee
+     */
 
     public Boolean estado() {
         return this.amFm;
     }
+    
+    /**
+     * @param onOff cambia de true a false
+     * @return el valor que cambio
+     */
+    
 
     public void onOff() { //Cambia de true a false y viceversa
         if (this.onOff) {
@@ -50,6 +75,11 @@ public class Radio implements RadioInterface {
             this.onOff = true;
         }
     }
+    /**
+     * @param cambiarFrecuencia verifica cual es el valor que posee la variable
+     * @return el valor de la frecuencia, donde si es AM regresa un valor y al ser FM otro.
+     * 
+     */
 
     public void cambiarFrecuencia() {
         if (this.amFm) {
@@ -60,6 +90,11 @@ public class Radio implements RadioInterface {
             this.estacionActual = 87.9; //Al ser puesto en FM se reinicia con la estacion de inicio como la actual
         }
     }
+    
+    /**
+     * @param avanzar el valor de la estacion actual
+     * @return el nuevo valor de la estacion actual
+     */
 
     public void avanzar() {
         if (this.amFm) {
@@ -70,6 +105,12 @@ public class Radio implements RadioInterface {
         returnToPrincipalStation();
     }
 
+    
+    /**
+     * @param returnToPrincipalStation comienza el cambio de estacion en su estacion incial
+     * @return el valor incial de la radio
+     * 
+     */
     private void returnToPrincipalStation() { //Revisa si la estacion actual supera su limite superior y lo regresa al inicial
         if (this.amFm) {
             if (this.estacionActual > 107.9) {
@@ -81,15 +122,23 @@ public class Radio implements RadioInterface {
             }
         }
     }
+    /**
+     * @param guardar almacena el valor deseado
+     * @return el valor guardado
+     */
 
-    public void guardar(int boton) { //Guarda la estacion actual en la posicion del boton seleccionado a la lista indicada
-        boton -= 1;
+    public void guardar(final int boton) { //Guarda la estacion actual en la posicion del boton seleccionado a la lista indicada
         if (this.amFm) {
             this.estacionesFm.set(boton, this.estacionActual);
         } else {
             this.estacionesAm.set(boton, this.estacionActual);
         }
     }
+    
+   /**
+    * @param seleccioEmisora toma el valor de la emisora
+    * @return el valor del boton
+    */
 
     public void seleccionarEmisora(int boton) { //Toma el valor del boton seleccionado en la lista de estaciones y la coloca en la actual
         if (this.amFm) {
